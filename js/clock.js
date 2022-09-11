39b8045fea82bf204016b3415dd032687409e1e4
dayglow0926 <dayglow0926@gmail.com>
2022-09-11 23:37:54 +0900

    Feat: 현재 시간 추가

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,4 @@
-const clock = document.querySelector(".clock");
+const clockList = document.querySelectorAll(".common-clock");
 
 function nowClock() {
   const date = new Date();
@@ -6,9 +6,13 @@ function nowClock() {
   const minutes = String(date.getMinutes()).padStart(2, 0);
   const seconds = String(date.getSeconds()).padStart(2, 0);
 
-  const nowClock = `${hours} : ${minutes} : ${seconds}`;
-  clock.innerHTML = nowClock;
+  clockList.forEach((item) => {
+    item.children[0].innerHTML = `${hours}:${minutes}`;
+    if (item.children.length > 1) {
+      item.children[1].innerHTML = `${seconds}`;
+    }
+  });
 }
 
-//nowClock();
+nowClock();
 setInterval(nowClock, 1000);
